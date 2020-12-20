f7edc1c60d04a912f9839cbee0b6936349731fb2
Yui Uehara <yuitaka@protonmail.com>
2020-12-20 10:19:13 -0800

    be a little kind

--- banme/banme.py
+++ banme/banme.py
@@ -2,6 +2,7 @@ from typing import Any
 
 import discord
 from redbot.core import commands
+from redbot.core.utils.predicates import MessagePredicate
 
 Cog: Any = getattr(commands, "Cog", object)
 
@@ -19,4 +20,11 @@ class BanMe(Cog):
         """
         Does what it says on the tin, bans yourself.
         """
-        await ctx.guild.ban(ctx.author, reason="You literally banned yourself")
+        try:
+            pred = MessagePredicate.yes_or_no()
+            await self.bot.wait_for("message", check=MessagePredicate.same_context(ctx))
+            await ctx.send("Are you sure about banning yourself? Answer with a `Yes` or a `No`")
+            if pred.result is True:
+                await ctx.guild.ban(ctx.author, reason="You literally banned yourself")
+        except Exception as e:
+            await ctx.send("Yeah I don't have perms to do that.")
